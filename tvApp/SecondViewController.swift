2a5c281bb8dd575ca5cda23c72caf8724af6579d
Ben Chatelain <ben@octop.ad>
2015-09-09 21:35:01 -0600

    Add tvOS tabbed app template

---
+++ tvApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tvApp
+//
+//  Created by Ben Chatelain on 9/9/15.
+//  Copyright © 2015 Ben Chatelain. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
